50afcfdb58d33a4ba45da0014f0a64d705eb4276
kjunwo <kangjunwoo02@naver.com>
2024-10-15 15:50:26 +0900

    느낌표추가

--- Test1/src/Hello.java
+++ Test1/src/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
     //이 클래스는 Hello World를 출력하는 클래스입니다.
-         System.out.println("Hello World");
+         System.out.println("Hello World!!");
         System.out.println("1");
         System.out.println("2");
         System.out.println("3");
