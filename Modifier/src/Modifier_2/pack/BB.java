a2661877333a63edea04bd82d85269474ff8c443
kjunwo <kangjunwoo02@naver.com>
2024-11-01 16:08:14 +0900

    메서드

---
+++ Modifier/src/Modifier_2/pack/BB.java
@@ -0,0 +1,5 @@
+package Modifier_2.pack;
+// 디폴트 클래스 : 일반적이지 않으므로 사용안함
+class BB {
+    // 기본생성자 만들지 않음
+}
---
+++ Modifier/src/Modifier_2/pack/CC.java
@@ -0,0 +1,9 @@
+package Modifier_2.pack;
+
+public class CC {
+    // 기본생성자 만듬
+    // public 클래스인데 생성자가 default
+    CC() {
+
+    }
+}
