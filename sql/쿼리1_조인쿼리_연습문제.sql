e5e9c900082a8dca91c8e2e3103e6fb3b38963a8
kjunwo <kangjunwoo02@naver.com>
2024-12-02 14:09:39 +0900

    쿼리sql

--- sql/쿼리1_조인쿼리_연습문제.sql
+++ sql/쿼리1_조인쿼리_연습문제.sql
@@ -42,3 +42,36 @@ inner join 사원
 on 주문.사원번호 = 사원.사원번호
 where 주문일 between '2020-04-01' and '2020-04-30'
 order by 주문일;
+
+-- 7. 마일리지 등급별로 고객수 조회
+select 등급명, count(*) as 고객수 
+from 고객
+inner join 마일리지등급
+on 마일리지 between 하한마일리지 and 상한마일리지
+group by 등급명
+order by 등급명;
+
+-- 8. 주문번호 'H0249'를 주문한 고객의 모든 정보 조회
+select 고객.*
+from 고객
+inner join 주문
+on 고객.고객번호 = 주문.고객번호
+where 주문.주문번호 = 'H0249';
+
+-- 9. 2020년 4월 9일에 주문한 고객의 모든 정보 조회
+select 고객.*
+from 주문
+inner join 고객
+on 주문.고객번호 = 고객.고객번호
+where 주문일 = '2020-04-09';
+
+-- 10. 도시별로 주문금액합을 보이되 주문금액합이 많은 상위 5개 도시에 대한 결과 조회
+select 도시, sum(주문수량 * 단가) as 주문금액합
+from 주문
+inner join 고객
+on 주문.고객번호 = 고객.고객번호
+inner join 주문세부
+on 주문.주문번호 = 주문세부.주문번호
+group by 도시
+order by 주문금액합 desc 
+limit 5;
