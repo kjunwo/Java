57a99f43bb88900526b1a2798e672deb7b0e90e3
kjunwo <kangjunwoo02@naver.com>
2024-12-03 16:21:34 +0900

    쿼리.sql

--- sql/쿼리2_서브쿼리.sql
+++ sql/쿼리2_서브쿼리.sql
@@ -49,26 +49,33 @@ group by 도시;
 
 -- #3. select절
 -- 고객번호, 담당자명, 고객의 최종주문일 조회
+-- 하나의 컬럼 값만 반환하는 서브쿼리를 스칼라 서브쿼리(Scalar SubQuery)
+-- select절에 사용되는 서브쿼리는 모두 스칼라 서브쿼리임!
 select 고객번호, 담당자명, 
 	(select max(주문일)from 주문 where 주문.고객번호 = 고객.고객번호) as 최종주문일
 from 고객;
 
 -- 위 쿼리를 from절의 서브쿼리로 변경하면?? (인라인뷰 방식)
-select 고객번호, 담당자명, 최종주문일
+select 고객.고객번호, 담당자명, 최종주문.최종주문일
 from 고객
-inner join (select 주문일, max(주문일) as 최종주문일
+left outer join (select 고객번호, max(주문일) as 최종주문일
 from 주문
-group by 주문일) as 최종주문일;
-on 고객.도시 = 도시요약.도시;
+group by 고객번호) as 최종주문 -- 인라인뷰에는 반드시 별칭이 필요함 
+on 고객.고객번호 = 최종주문.고객번호;
 
 -- 위 쿼리를 일반적인 조인쿼리로 변경하면??
-select 고객번호, 담당자명, 최종주문일
-from 고객 
-inner join 주문
-on 고객.고객번호 = 주문.고객번호;
-where 최종주문일;
+select 고객.고객번호, 담당자명, max(주문일) as 최종주문일 
+from 고객
+left outer join 주문
+on 고객.고객번호 = 주문.고객번호
+group by 고객.고객번호, 담당자명;
 
-
+-- Having절에도 서브쿼리를 사용할 수 있음. 단, 사용원리가 where절과 유사하므로 따로 다루지 않음
+-- 고객 전체의 평균마일리지보다 평균마일리지가 큰 도시
+select 도시, avg(마일리지) as 평균마일리지
+from 고객
+group by 도시
+having avg(마일리지) > (select avg(마일리지) from 고객);
 
 
 
