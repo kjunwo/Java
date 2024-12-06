e607ba27b9c4010065f034d38a69eba70658ad90
kjunwo <kangjunwoo02@naver.com>
2024-12-06 17:09:18 +0900

    Codingtest

---
+++ CodingTest/src/CodingTest_2/PrintedBook.java
@@ -0,0 +1,34 @@
+package CodingTest_2;
+
+public class PrintedBook extends Book{
+    int pageCount;
+    boolean isLent;
+
+    public PrintedBook (String title, String author, int pageCount) {
+        super(title,author);
+        this.pageCount = pageCount;
+    }
+    boolean isLent() {
+        return isLent;
+    }
+    void lend() {
+        if(isLent) {
+            System.out.println("title"+"은 이미 대출중입니다.");
+        }else {
+            System.out.println("title"+"은 성공적으로 대출 처리되었습니다.");
+        }
+    }
+    public void returnBook(){
+        if (isLent) {
+            System.out.println("title"+ "은 대출중이 아닙니다.");
+        }else {
+            System.out.println("title" + "은 성공적으로 반납되었습니다.");
+        }
+    }
+    @Override
+    void displayDetails() {
+        System.out.println("Printed Book" + "-" + "title : " + title  +  "," +
+                "author : " + author + "," +
+                "pageCount : " + pageCount + "," + "isLent : " + isLent);
+    }
+}
